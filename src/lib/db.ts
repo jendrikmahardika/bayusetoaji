14038cecd0afef3b2b838de00a6b4c769e44c891
jendrikmahardika <jendrikmahardika@gmail.com>
2025-12-12 20:35:41 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,16 @@
-import db from '@/lib/db';
+// src/lib/db.ts
+import { PrismaClient } from '@prisma/client';
 
-export default db;
+// Buat caching Prisma supaya ga bikin multiple instances di dev
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'], // optional, bisa dihapus kalau ga mau log query
+  });
+
+// Hanya assign ke global di development supaya hot reload aman
+if (process.env.NODE_ENV !== 'production') global.prisma = db;
